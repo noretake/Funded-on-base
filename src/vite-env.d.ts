49091057fc9c5b92d68faaa4e70d18f52b04d98c
noretake <42299902+noretake@users.noreply.github.com>
2025-10-24 15:33:46 +0300

    Add files via upload

---
+++ src/vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+interface Window {
+  ethereum?: {
+    isMetaMask?: boolean;
+    request: (args: { method: string; params?: any[] }) => Promise<any>;
+    on: (event: string, callback: any) => void;
+    removeListener: (event: string, callback: any) => void;
+  };
+}
